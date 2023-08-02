680340d8f4def6f30d105aed1c3e2a51c0286c6d
svetit-sa <115215274+svetit-sa@users.noreply.github.com>
2023-08-02 12:06:25 +0300

    Update сlick_on_an_element.py

--- сlick_on_an_element.py
+++ сlick_on_an_element.py
@@ -3,9 +3,9 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-driver.get("https://around-v1.en.practicum-services.com/")
+driver.get("https://around-v1.es.practicum-services.com/")
 
-# Find the button and click on it
+# Buscar el botón y hacer clic en él
 driver.find_element(...)...
 
 driver.quit()
