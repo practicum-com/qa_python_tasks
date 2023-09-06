03be80caa887866b4bca941b7e88261f027eabb7
svetit-sa <115215274+svetit-sa@users.noreply.github.com>
2023-09-06 11:27:15 +0300

    Update сlick_on_an_element.py

--- сlick_on_an_element.py
+++ сlick_on_an_element.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-driver.get("https://around-v1.en.practicum-services.com/")
+driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=en")
 
 # Find the button and click on it
 driver.find_element(...)...
