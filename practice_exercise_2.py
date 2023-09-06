1177d4229f7414494e61039c501a8d48c69c68ad
svetit-sa <115215274+svetit-sa@users.noreply.github.com>
2023-09-06 11:26:41 +0300

    Update practice_exercise_2.py

--- practice_exercise_2.py
+++ practice_exercise_2.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
-driver.get("https://around-v1.en.practicum-services.com/")
+driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=en")
 
 # Log in
 ...
